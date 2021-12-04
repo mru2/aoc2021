7c5a07d5682683b7834b61d54e963e3b9a1c72d5
mru2 <david.ruyer+github@gmail.com>
2021-12-05 00:29:39 +0100

    day3

--- lib/aoc.ex
+++ lib/aoc.ex
@@ -9,10 +9,22 @@ defmodule Aoc do
   def parse_numbers(input), do: input |> parse_lines() |> Enum.map(&String.to_integer/1)
 
   @spec pairs(any) :: [{any, any}]
-  def pairs([h1, h2|rest]), do: [{h1, h2}|pairs([h2|rest])]
+  def pairs([h1, h2 | rest]), do: [{h1, h2} | pairs([h2 | rest])]
   def pairs(_), do: []
 
   @spec triplets(any) :: [{any, any, any}]
-  def triplets([h1, h2, h3|rest]), do: [{h1, h2, h3}|triplets([h2, h3|rest])]
+  def triplets([h1, h2, h3 | rest]), do: [{h1, h2, h3} | triplets([h2, h3 | rest])]
   def triplets(_), do: []
+
+  @spec transpose([list]) :: [list]
+  def transpose(rows), do: List.zip(rows) |> Enum.map(&Tuple.to_list/1)
+
+  def tally(list),
+    do: list |> Enum.group_by(& &1) |> Enum.map(fn {val, elems} -> {val, length(elems)} end)
+
+  def most_frequent(list),
+    do: list |> tally() |> Enum.max_by(fn {_val, count} -> count end) |> elem(0)
+
+  def least_frequent(list),
+    do: list |> tally() |> Enum.min_by(fn {_val, count} -> count end) |> elem(0)
 end
---
+++ lib/aoc/day03.ex
@@ -0,0 +1,24 @@
+defmodule Aoc.Day03 do
+  import Aoc
+  # import Enum
+
+  def parse(input) do
+    input
+    |> parse_lines()
+    |> Enum.map(&(String.split(&1, "", trim: true)))
+    |> transpose
+  end
+
+  def bits_to_int(bits), do: bits |> Enum.join("") |> String.to_integer(2)
+
+  def run(data) do
+    tops = data |> Enum.map(&most_frequent/1) |> bits_to_int()
+    bottoms = data |> Enum.map(&least_frequent/1) |> bits_to_int()
+
+    tops * bottoms
+  end
+
+  def bonus(data) do
+    data
+  end
+end
